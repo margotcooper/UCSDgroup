6ea1b6df6d0c0ceab3390fc694b85b5a6d86e50c
JC Berry <jcharlesberry@gmail.com>
2022-07-19 22:15:19 -0700

    initial commit

---
+++ Assets/js/index.js
@@ -0,0 +1,28 @@
+var searchTextBoxEl = document.getElementById('searchbox');
+var searchOptionsEl = document.getElementById('searchoptions');
+var searchBtnEl = document.getElementById('searchbtn');
+
+searchBtnEl.addEventListener('submit', sendToResultsPg);
+function sendToResultsPg(e){
+    var searchPhrase = searchTextBoxEl.textContent;
+    var searchOption = searchOptionsEl.selectedIndex;
+    if(searchOption !== ''){
+        location.replace('search-results.html?q=' + searchPhrase + '&format=' + searchOption);
+    }else{
+        location.replace('search-results.html?q=' + searchPhrase + '&format=');
+    }
+}
+
+
+
+
+
+
+
+
+
+/* A form with a text input field to capture a search query and an option
+select dropdown to capture the format of the search query. The options in
+the dropdown should be a list of the possible format values listed in the
+[Library of Congress API documentation on requests]
+(https://libraryofcongress.github.io/data-exploration/requests.html#format).*/
